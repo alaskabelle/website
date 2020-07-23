5f2a6489c1c219d8de02a1f75ef7060eb8e49378
alaskabelle <66294922+alaskabelle@users.noreply.github.com>
2020-07-23 00:48:15 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-  let toggle = document.querySelector('.madeby-toggle')
-  let madeby = document.querySelector('.madeby')
-
-  toggle.addEventListener('click', () => {
-    madeby.classList.toggle('hidden')
-  })
-})
